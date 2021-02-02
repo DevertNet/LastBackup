bbaa3e2a88a55596dee91ac8c77da1c70659a311
Leon <l.weickert@loewenstark.com>
2021-02-02 13:33:47 +0100

    fix: remove demo data in contextes

--- src/frontend/context/StoragePasswordContext.tsx
+++ src/frontend/context/StoragePasswordContext.tsx
@@ -5,8 +5,8 @@ type State = string;
 const StoragePasswordContext = React.createContext<[State, React.Dispatch<React.SetStateAction<State>>] | null>(null);
 
 export default function StoragePasswordContextProvider(props: { children: React.ReactNode }) {
-    //const [password, setPassword] = useState('');
-    const [password, setPassword] = useState('asdasd');
+    const [password, setPassword] = useState('');
+    // const [password, setPassword] = useState('asdasd');
 
     return <StoragePasswordContext.Provider value={[password, setPassword]}>{props.children}</StoragePasswordContext.Provider>
 };
